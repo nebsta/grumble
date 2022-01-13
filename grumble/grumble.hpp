21e91a51708b119920efd6104891110d427e1e5c
Benjamin Wallis <ben.nat.wallis@gmail.com>
2022-01-13 12:59:22 +0100

    first commit

---
+++ grumble/grumble.hpp
@@ -0,0 +1,21 @@
+//
+//  grumble.hpp
+//  grumble
+//
+//  Created by Benjamin Wallis on 13/01/2022.
+//
+
+#ifndef grumble_
+#define grumble_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class grumble
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
